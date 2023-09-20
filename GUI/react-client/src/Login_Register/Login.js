97c75195eb51a29b816742e2bee89b908a32e9c4
tamir057 <tas.amir57@gmail.com>
2023-09-19 22:42:34 -0400

    Login finished -ish

--- GUI/react-client/src/Login_Register/Login.js
+++ GUI/react-client/src/Login_Register/Login.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react'
+import hydroponics from './hydroponics_img.png'
+import hydroponics2 from './hydroponics_img2.png'
+import { Link } from 'react-router-dom';
+
 function Login() {
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}])    
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+  const [password, setPassword] = useState("")
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState("")
+
   useEffect(() => {
     fetch("/members").then(
       res => res.json()
@@ -12,16 +22,52 @@ function Login() {
     }
   )
 }, [])
-  return (
-    <div className="Login">
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
-    </div>
-  );
+
+const onButtonClick = () => {
+  // update this function later
+}
+
+return <div class='login'>
+<div class={'login'} className={"mainContainer"}>
+  <div className={"titleContainer"}>
+      <div>Login To</div>
+      </div>
+      <div className={"subtitleContainer"}> GROWTH Web App</div>
+  <br />
+  <div className={"inputContainer"}>
+  <div className={"subtitle2Container"}> Email</div>
+      <input
+          value={email}
+          placeholder="Enter your email here"
+          onChange={ev => setEmail(ev.target.value)}
+          className={"inputBox"} />
+      <label className="errorLabel">{emailError}</label>
+  </div>
+  <br />
+  <div className={"inputContainer"}>
+  <div className={"subtitle2Container"}> Password </div>
+      <input
+          value={password}
+          placeholder="Enter your password here"
+          onChange={ev => setPassword(ev.target.value)}
+          className={"inputBox"} />
+      <label className="errorLabel">{passwordError}</label>
+  </div>
+  <br />
+  <div className={"inputContainer"}>
+      <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Log In"} />
+  </div>
+  <div className={"subtitle3Container"}> <p>Don't Have an Account?  
+  <Link to='/register'> Register</Link>  </p>
+  </div>
+  </div>
+  <div><img style={{ align: 'right' }} src={hydroponics2} height={500} width={800}  /></div>
+  </div>
+
+
 }
 export default Login;
