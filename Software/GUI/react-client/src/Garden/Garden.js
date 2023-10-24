2b69a4f6e5c77b931eadfd02ed090d099bdd781d
tamir057 <tas.amir57@gmail.com>
2023-10-24 01:51:51 -0400

    Popup 1 -- FIX MERGE ISSUES

---
+++ Software/GUI/react-client/src/Garden/Garden.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css";
+import PlotList from "./plot-list";
+import Select from './Select'; // Import the AddPlotModal component
+
+function Garden() {
+  const [showAddPopup, setShowAddPopup] = useState(false);
+
+  const openAddPopup = () => {
+    setShowAddPopup(true);
+  };
+
+  const closeAddPopup = () => {
+    setShowAddPopup(false);
+  };
+
+  return (
+    <div>
+      <div className="nav-padding bg-grey-c wide row">
+        <div className="col-11 bg-secondary-green container mt-4 wide round-10">
+          <div className="row">
+            <div className="col-6">
+              <h3 className="p-2">Farm Layout</h3>
+            </div>
+            <div className="col-6 float-end">
+              <div className="row">
+                <div className="col-2"></div>
+                <button className="m-2 col-3 btn button-primary round-15">Calibrate</button>
+                <button className="m-2 col-3 btn button-primary round-15">Run</button>
+                <button className="m-2 col-3 btn button-primary round-15" onClick={openAddPopup}>Add</button>
+              </div>
+            </div>
+          </div>
+          <PlotList />
+        </div>
+      </div>
+      <Select showModal={showAddPopup} handleClose={closeAddPopup} />
+    </div>
+  );
+}
+
+export default Garden;
---
+++ Software/GUI/react-client/src/Garden/Select.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Select({ showModal, handleClose }) {
+  const modalStyle = {
+    position: 'fixed',
+    right: 0, // Adjust the right position as needed
+    top: 0,
+    bottom: 0,
+    width: '300px', // Adjust the width as needed
+    backgroundColor: 'white',
+    zIndex: 1050,
+    display: showModal ? 'block' : 'none',
+  };
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+    // You can add code here to perform the search based on the query.
+  };
+
+  const contentStyle = {
+    flex: 1,
+  };
+
+  const footerStyle = {
+    marginTop: 'auto',
+  };
+
+  return (
+    <div style={modalStyle}>
+      <div className="modal-content">
+        <div className="modal-header">
+          <button type="button" className="close" onClick={handleClose}>
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <h5 className="subtitle3Container">Select Plant:</h5>
+        
+        <div className="modal-body" style={contentStyle}>
+          {/* Add a search bar input element */}
+          <input
+            type="text"
+            placeholder="Search for plant..."
+            value={searchQuery}
+            onChange={handleSearch}
+          />
+          {/* Add your content for selecting options here */}
+        </div>
+        <div className={"subtitle2Container"}>  
+        <Link to='/register'> Create a New Plant Profile</Link> 
+        </div>
+        <div className="modal-footer" style={footerStyle}>
+          <div className={"inputButton"}>
+            <input
+              className={"inputButton"}
+              type="button"
+              value={"Save"} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Select;
