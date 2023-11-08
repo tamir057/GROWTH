87d692a97a44ad8d0c2c9c37b81f98defaf8ae24
tamir057 <tas.amir57@gmail.com>
2023-11-08 17:15:00 -0500

    Progress on second popup

--- Software/GUI/react-client/src/Garden/Select.js
+++ Software/GUI/react-client/src/Garden/Select.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./select_index.css";
 
-function Select({ showModal, handleClose }) {
-  let firstScreen = true; 
+const Select = ({ showModal, handleClose }) => {
+
+  let firstScreen = false; 
+  
   const modalStyle = {
     position: 'fixed',
     right: 0, // Adjust the right position as needed
     top: 0,
     bottom: 0,
-    width: '300px', // Adjust the width as needed
+    width: '400px', // Adjust the width as needed
     backgroundColor: 'white',
     zIndex: 1050,
     display: showModal ? 'block' : 'none',
@@ -18,7 +20,7 @@ function Select({ showModal, handleClose }) {
 
   const handleSearch = (event) => {
     setSearchQuery(event.target.value);
-    // You can add code here to perform the search based on the query.
+    // Add code
   };
 
   const contentStyle = {
@@ -30,6 +32,8 @@ function Select({ showModal, handleClose }) {
   };
 
   return (
+    <div>
+    {firstScreen &&
     <div style={modalStyle}>
       <div className="modal-content">
         <div className="modal-header">
@@ -61,8 +65,69 @@ function Select({ showModal, handleClose }) {
           </div>
         </div>
       </div>
-    </div>
+    </div>}
+    {!firstScreen && 
+    <div style={modalStyle}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" onClick={handleClose}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body" style={contentStyle}>
+              <h5 className="subtitle3Container">Last Run Time:</h5>
+              <p>Last Run Time: [Insert Time Here]</p>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Temp</th>
+                    <th>pH</th>
+                    <th>Water Conductivity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>[Temp Value]</td>
+                    <td>[pH Value]</td>
+                    <td>[Water Conductivity Value]</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="modal-footer" style={footerStyle}>
+              <div className={"saveButton"}>
+                <input
+                  className={"saveButton"}
+                  type="button"
+                  value={"Save"} />
+              </div>
+            </div>
+          </div>
+        </div>}
+  </div>
+    
+   
   );
 }
-
 export default Select;
+
+
+
+// //import FirstPopUp from "./FirstPopUp";
+// import SecondPopUp from "./SecondPopUp";
+
+
+// const Select = () => {
+//   let firstScreen = true; 
+//   return (
+
+//     <div>
+//       {firstScreen && <FirstPopUp/>}
+//       {!firstScreen && <SecondPopUp/>}
+//     </div>
+   
+//   );
+// }
+// export default Select;
+
+
