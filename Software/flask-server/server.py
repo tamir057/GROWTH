586ba94c54721a420ecd15de008600a7e521bfba
Rida Jawed <rida.jawed@gmail.com>
2023-11-10 23:12:04 -0500

    back end mock data

--- Software/flask-server/server.py
+++ Software/flask-server/server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import os, json
 
 from flask_cors import CORS
 
@@ -9,6 +10,54 @@ CORS(app)
 @app.route("/members")
 def members():
     return {"members" : ["Rida", "Tracy", "Tas"]}
+
+plots_file_path = os.path.join(os.path.dirname(__file__), 'plots.json')
+
+def read_plots_from_file():
+    with open(plots_file_path, 'r') as file:
+        data = file.read()
+        return data
+    
+def write_plots_to_file(data):
+    with open(plots_file_path, 'w') as file:
+        file.write(data)
+
+@app.route('/api/getPlots', methods=['GET'])
+def get_data():
+    try:
+        # Read data from 'plots.json' file and parse it as JSON
+        plot_array = json.loads(read_plots_from_file())
+        return jsonify(plot_array)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+@app.route('/api/addPlots', methods=['POST'])
+def add_data():
+    try:
+        data = request.json  # Assuming the request contains JSON data
+        plots_number = data.get('plots_number', 0)  # Extract the number of plots from the JSON data
+        plot_array = json.loads(read_plots_from_file())
+        size = len(plot_array)
+        # plots_number = 5
+        # TODO: get the number of plots 
+        new_data = []
+        for i in range(plots_number):
+            # You can add any value or object to the list here
+            new_data.append({
+                "_id": f"{i + 1 + size}",
+                "plot_number": f"{i + 1 + size}",  # Adjust the plot_number as needed
+                "plant": ""
+            })
+
+        # Extend the existing array with the new data
+        plot_array.extend(new_data)
+
+        # Write the updated array back to the file
+        write_plots_to_file(json.dumps(plot_array))
+
+        return jsonify({'success': True})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
