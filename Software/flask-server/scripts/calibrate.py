9b2f361593ffc7d25f9cba571bb9ce9f2a750906
tamir057 <tas.amir57@gmail.com>
2023-11-16 14:52:54 -0500

    calibrate changes with firmware team

--- Software/flask-server/scripts/calibrate.py
+++ Software/flask-server/scripts/calibrate.py
@@ -9,6 +9,13 @@ import serial
 import threading
 
 received_message = ""
+boundary_offset = 4000
+current_position = 0
+data = { 
+    "1": 0,
+    "2" : 0,
+    "3" : 0
+}
 
 def receive_data(ser):
     while True:
@@ -71,32 +78,52 @@ def fiducial_detection():
                 cX = int((topLeft[0] + bottomRight[0]) / 2.0)
                 cY = int((topLeft[1] + bottomRight[1]) / 2.0)
                 cv2.circle(im, (cX, cY), 4, (0, 0, 255), -1) 
-                if (cX > 157 and cX < 163):
+                if (cX > 157 and cX < 163) :
                     cv2.imshow("Middle Frame", im)
-                    # TODO: message syntax 
-                    message = "STOP_MOTOR:0"
-                    ser.write(message.encode('utf-8')+ b'\n')
-                    message = "READ_STEPS:0,0"
-                    ser.write(message.encode('utf-8')+ b'\n')
                     time.sleep(0.1)
+                    return 
                 # draw the ArUco marker ID on the frame
             cv2.putText(im, str(markerID),(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)
         # show the output frame
         cv2.imshow("Frame", im)
         key = cv2.waitKey(1) & 0xFF
-            # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
+        if key == ord('q'):
             break
 
-#TODO: message = ?
+#TODO: CREATE HELPER FOR ACKS !!! 
 def calibrate(total_fiducials):
-        message = "MOVE_MOTOR_CONT:1,1"  # send up first 
-        message = "MOVE_MOTOR_CONT:0,1"
+        message = "MOVE_MOTOR_CONT:1,0"  # send up first 
         ser.write(message.encode('utf-8')+ b'\n')
-        while (received_message != "TEMPORARY"):
+        # wait for ack -- while received mssg is not the ack 
+        # set state of z motor as 'up' -- store locally? 
+        message = "MOVE_MOTOR_CONT:0,1" # send to right  
+        ser.write(message.encode('utf-8')+ b'\n')
+        while (received_message != "ACK"): # TODO: ack mssg that at limit swirtch
+            pass 
+        message = "MOVE_MOTOR_STEPS:0,0," + boundary_offset # TODO: verify string formatting
+        ser.write(message.encode('utf-8')+ b'\n')
+        while (received_message != "ACK"): # TODO: ack mssg that it moved the offset val 
+            pass 
+        message = "SET_ZERO_POS:0" 
+        ser.write(message.encode('utf-8')+ b'\n')
+        while (received_message != "ACK"): # TODO: ack mssg for zero state
+            pass 
+        
+        while (received_message != "ACK"): # TODO: at limit switch mssg
             message = "MOVE_MOTOR_CONT:0,0"
             ser.write(message.encode('utf-8')+ b'\n')
             fiducial_detection()
+            message = "STOP_MOTOR:0"
+            ser.write(message.encode('utf-8')+ b'\n')
+            # add wait for ack 
+            message = "RETURN_POS:0"
+            ser.write(message.encode('utf-8')+ b'\n')
+            current_position = received_message.split(":")[1]
+            fiducials_detected += 1
+            data[fiducials_detected + ""] = current_position
+
+        message = "MOVE_MOTOR_STEPS:0,0," + boundary_offset   # moving to left 
+        ser.write(message.encode('utf-8')+ b'\n')
         
         if fiducials_detected < total_fiducials:
             fiducials_detected = 0
