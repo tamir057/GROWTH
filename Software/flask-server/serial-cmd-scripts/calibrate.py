ceb10a8f550564c0e8ec95900a9f82523bc76dad
tamir057 <tas.amir57@gmail.com>
2023-11-16 19:42:35 -0500

    mostly working calibrate function

--- Software/flask-server/serial-cmd-scripts/calibrate.py
+++ Software/flask-server/serial-cmd-scripts/calibrate.py
@@ -9,7 +9,7 @@ import serial
 import threading
 import sys
 
-received_message = ""
+received_message = ["::"]
 boundary_offset = 4000
 current_position = 0
 data = { 
@@ -18,14 +18,16 @@ data = {
     "3" : 0
 }
 
-total_fiducials = sys.argv[1:]
+# total_fiducials = sys.argv[1:]
+total_fiducials = 3
 
 def receive_data(ser):
+    #global received_message
     while True:
         if ser.in_waiting > 0:
             line = ser.readline()
-            received_message = line.decode('utf-8').rstrip()
-            print(received_message)
+            received_message[0] = line.decode('utf-8').rstrip()
+            print(f"full received message {received_message[0]}", "  from received")
 ser = serial.Serial('/dev/ttyACM0', 115200)
 receive_thread = threading.Thread(target=receive_data, args=(ser,))
 receive_thread.daemon = True
@@ -49,6 +51,9 @@ picam2.start()
 fiducials_detected = 0
 
 def fiducial_detection():
+    #im = picam2.capture_array()
+    #im = cv2.cvtColor(im, cv2.COLOR_BGRA2BGR)
+    #cv2.imshow("image", im)
     while True:
         im = picam2.capture_array()
         #im = imutils.resize(im, width=1000)
@@ -82,85 +87,127 @@ def fiducial_detection():
                 cY = int((topLeft[1] + bottomRight[1]) / 2.0)
                 cv2.circle(im, (cX, cY), 4, (0, 0, 255), -1) 
                 if (cX > 157 and cX < 163) :
-                    cv2.imshow("Middle Frame", im)
+                    #cv2.imshow("Middle Frame", im)
+                    print("found a fiducial")
                     time.sleep(0.1)
                     return 
                 # draw the ArUco marker ID on the frame
             cv2.putText(im, str(markerID),(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)
         # show the output frame
-        cv2.imshow("Frame", im)
+        #cv2.imshow("Frame", im)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
 
 # helper to wait for acks 
 def wait_for_ack(message):
-    received_message = received_message.split(":")[1]
-    while (received_message != message):
-        pass 
+    
+    #global received_message
+    #if (received_message != "")
+    #parsed_message = received_message.split(":")[1]
+    parsed_message = "::"
+    print(received_message[0], " from beginning of wait")
+    #print(parsed_message)
+    while (parsed_message != message):
+        #global received_message 
+        parsed_message = received_message[0].split(":")[1]
+        pass
+    #print(received_message[0])
+    print(parsed_message, "from ack") 
+
+
+def send_serial(command):
+    #global receieved_message
+    received_message[0] = "::"
+    ser.write(command.encode('utf-8')+ b'\n') 
 
 #TODO: CREATE HELPER FOR ACKS !!! 
 def calibrate():
+    fiducials_detected = 0
+    #global received_message
     message = "MOVE_MOTOR_CONT:1,0"  # send up first 
-    ser.write(message.encode('utf-8')+ b'\n')
+    send_serial(message)
+    #ser.write(message.encode('utf-8')+ b'\n')
     wait_for_ack("LIMIT_SWITCH_TRIGGERED") #ack mssg that at limit switch
-    if received_message.split(":")[2] != 'UP':
+    if received_message[0].split(":")[2] != 'UP':
+        print(f"this is the direction {received_message[0].split(':')[2]}")
         raise ValueError("wrong direction!!!!!!! should be up")
+    print("up completed")
+    #received_message = "::"
     # TODO: set state of z motor as 'up' -- store locally? 
     message = "MOVE_MOTOR_CONT:0,1" # send to right  
-    ser.write(message.encode('utf-8')+ b'\n')
+    send_serial(message)
+    #ser.write(message.encode('utf-8')+ b'\n')
     wait_for_ack("LIMIT_SWITCH_TRIGGERED")
-    if received_message.split(":")[2] != 'RIGHT':
+    if received_message[0].split(":")[2] != 'RIGHT':
         raise ValueError("wrong direction!!!!!!! should be right")
     # TODO: set state of z motor as 'up' -- store locally? 
     message = f"MOVE_MOTOR_STEPS:0,0,{boundary_offset}" # TODO: verify string formatting
-    ser.write(message.encode('utf-8')+ b'\n')
-    wait_for_ack("LIMIT_SWITCH_TRIGGERED")
-    if received_message.split(":")[2] != 'LEFT':
-        raise ValueError("wrong direction!!!!!!! should be left")
+    send_serial(message)
+    #ser.write(message.encode('utf-8')+ b'\n')
+    wait_for_ack("MOVE_MOTOR_STEPS")
+    #if received_message[0].split(":")[2] != 'LEFT':
+    #    raise ValueError("wrong direction!!!!!!! should be left")
     message = "SET_ZERO_POS:0" 
-    ser.write(message.encode('utf-8')+ b'\n')
+    send_serial(message)
+    #ser.write(message.encode('utf-8')+ b'\n')
     wait_for_ack("SET_ZERO_POS")
     
-    while (received_message != "LIMIT_SWITCH_TRIGGERED"): 
+    while (received_message[0] != "LIMIT_SWITCH_TRIGGERED"): 
         message = "MOVE_MOTOR_CONT:0,0"
-        ser.write(message.encode('utf-8')+ b'\n')
+        send_serial(message)
+        #ser.write(message.encode('utf-8')+ b'\n')
         fiducial_detection()
         message = "STOP_MOTOR:0"
-        ser.write(message.encode('utf-8')+ b'\n')
-        wait_for_ack("STOP_MOTOR")  #ack mssg that it moved the stopped the motor   
+        send_serial(message)
+        #ser.write(message.encode('utf-8')+ b'\n')
+        wait_for_ack("STOP_MOTOR")  #ack mssg that it moved the stopped the motor
+        send_serial(message)
+        wait_for_ack("STOP_MOTOR")
         message = "RETURN_POS:0"
-        ser.write(message.encode('utf-8')+ b'\n')
+        time.sleep(0.5)
+        send_serial(message)
+        #ser.write(message.encode('utf-8')+ b'\n')
         # while received message split[0]!= CURRENT_POS
-        while (received_message.split(":")[0]!= 'CURRENT_POSITION'): # TODO: ack mssg that it moved the offset val 
-            pass 
-        current_position = received_message.split(":")[1]
+        wait_for_ack("RETURN_POS")
+        #while (received_message[0].split(":")[1]!= 'RETURN__POS'): # TODO: ack mssg that it moved the offset val 
+        #    pass 
+        current_position = received_message[0].split(":")[2]
         fiducials_detected += 1
-        data[fiducials_detected + ""] = current_position
+        data[f"{fiducials_detected}"] = current_position
         time.sleep(1)
 
     message = f"MOVE_MOTOR_STEPS:0,0,{boundary_offset}"   # moving to left 
-    ser.write(message.encode('utf-8')+ b'\n')
+    send_serial(message)
+    wait_for_ack("MOVE_MOTOR_STEPS")
+    #ser.write(message.encode('utf-8')+ b'\n')
     # request current position and store that as outer boundary 
     # create helper for returning current position
+    message = "STOP_MOTOR:0"
+    send_serial(message)
+    wait_for_ack("STOP_MOTOR")
+
     message = "RETURN_POS:0"
-    ser.write(message.encode('utf-8')+ b'\n')
+    send_serial(message)
+    #ser.write(message.encode('utf-8')+ b'\n')
     # while received message split[0]!= CURRENT_POS
-    while (received_message.split(":")[0]!= 'CURRENT_POSITION'): # TODO: ack mssg that it moved the offset val 
-        pass 
+    wait_for_ack("RETURN_POS")
+    #while (received_message[0].split(":")[1]!= 'RETURN_POS'): # TODO: ack mssg that it moved the offset val 
+    #    pass 
     current_position = received_message.split(":")[1]
+    print(data)
     
         
-if __name__ == "__main__":
-    calibrate() 
-    if fiducials_detected < total_fiducials:
-        fiducials_detected = 0
-        calibrate()
+# if __name__ == "__main__":
+calibrate() 
+if fiducials_detected < total_fiducials:
+    fiducials_detected = 0
+    calibrate()
 
-    if fiducials_detected == total_fiducials:
-        print(data)
-    else:
-        raise ValueError("Failed to final all fiducials") 
+if fiducials_detected == total_fiducials:
+    print("All fiducials found!")
+else:
+    raise ValueError("Failed to final all fiducials") 
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
