e8d0f7ce5e591640b4b713318eaafb699ea78d1d
tamir057 <tas.amir57@gmail.com>
2023-11-17 12:58:13 -0500

    clean up calibrate

--- Software/flask-server/serial-cmd-scripts/calibrate.py
+++ Software/flask-server/serial-cmd-scripts/calibrate.py
@@ -51,10 +51,7 @@ picam2.start()
 fiducials_detected = 0
 
 def fiducial_detection():
-    #im = picam2.capture_array()
-    #im = cv2.cvtColor(im, cv2.COLOR_BGRA2BGR)
-    #cv2.imshow("image", im)
-    while True:
+    while received_message[0].split(":")[1] != "LIMIT_SWITCH_TRIGGERED":
         im = picam2.capture_array()
         #im = imutils.resize(im, width=1000)
         im = cv2.cvtColor(im, cv2.COLOR_BGRA2BGR)
@@ -95,107 +92,78 @@ def fiducial_detection():
             cv2.putText(im, str(markerID),(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)
         # show the output frame
         #cv2.imshow("Frame", im)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
+        # key = cv2.waitKey(1) & 0xFF
+        # if key == ord('q'):
+        #     break
 
 # helper to wait for acks 
 def wait_for_ack(message):
     
-    #global received_message
-    #if (received_message != "")
-    #parsed_message = received_message.split(":")[1]
     parsed_message = "::"
     print(received_message[0], " from beginning of wait")
-    #print(parsed_message)
+
     while (parsed_message != message):
-        #global received_message 
         parsed_message = received_message[0].split(":")[1]
         pass
-    #print(received_message[0])
     print(parsed_message, "from ack") 
 
 
 def send_serial(command):
-    #global receieved_message
     received_message[0] = "::"
     ser.write(command.encode('utf-8')+ b'\n') 
 
 #TODO: CREATE HELPER FOR ACKS !!! 
 def calibrate():
     fiducials_detected = 0
-    #global received_message
-    message = "MOVE_MOTOR_CONT:1,0"  # send up first 
-    send_serial(message)
-    #ser.write(message.encode('utf-8')+ b'\n')
+
+    send_serial("MOVE_MOTOR_CONT:1,0")   # send up first 
     wait_for_ack("LIMIT_SWITCH_TRIGGERED") #ack mssg that at limit switch
     if received_message[0].split(":")[2] != 'UP':
-        print(f"this is the direction {received_message[0].split(':')[2]}")
         raise ValueError("wrong direction!!!!!!! should be up")
-    print("up completed")
-    #received_message = "::"
+
     # TODO: set state of z motor as 'up' -- store locally? 
-    message = "MOVE_MOTOR_CONT:0,1" # send to right  
-    send_serial(message)
-    #ser.write(message.encode('utf-8')+ b'\n')
+    send_serial("MOVE_MOTOR_CONT:0,1") # send to right  
     wait_for_ack("LIMIT_SWITCH_TRIGGERED")
     if received_message[0].split(":")[2] != 'RIGHT':
         raise ValueError("wrong direction!!!!!!! should be right")
-    # TODO: set state of z motor as 'up' -- store locally? 
-    message = f"MOVE_MOTOR_STEPS:0,0,{boundary_offset}" # TODO: verify string formatting
-    send_serial(message)
-    #ser.write(message.encode('utf-8')+ b'\n')
+
+    send_serial(f"MOVE_MOTOR_STEPS:0,0,{boundary_offset}")
     wait_for_ack("MOVE_MOTOR_STEPS")
-    #if received_message[0].split(":")[2] != 'LEFT':
-    #    raise ValueError("wrong direction!!!!!!! should be left")
-    message = "SET_ZERO_POS:0" 
-    send_serial(message)
-    #ser.write(message.encode('utf-8')+ b'\n')
+
+    send_serial("SET_ZERO_POS:0" )
     wait_for_ack("SET_ZERO_POS")
     
-    while (received_message[0] != "LIMIT_SWITCH_TRIGGERED"): 
-        message = "MOVE_MOTOR_CONT:0,0"
-        send_serial(message)
-        #ser.write(message.encode('utf-8')+ b'\n')
+    while (received_message[0].split(":")[1] != "LIMIT_SWITCH_TRIGGERED"): 
+        send_serial("MOVE_MOTOR_CONT:0,0")
+
         fiducial_detection()
-        message = "STOP_MOTOR:0"
-        send_serial(message)
-        #ser.write(message.encode('utf-8')+ b'\n')
+
+        send_serial("STOP_MOTOR:0")
         wait_for_ack("STOP_MOTOR")  #ack mssg that it moved the stopped the motor
-        send_serial(message)
-        wait_for_ack("STOP_MOTOR")
-        message = "RETURN_POS:0"
         time.sleep(0.5)
-        send_serial(message)
-        #ser.write(message.encode('utf-8')+ b'\n')
-        # while received message split[0]!= CURRENT_POS
+
+        send_serial("RETURN_POS:0")
         wait_for_ack("RETURN_POS")
-        #while (received_message[0].split(":")[1]!= 'RETURN__POS'): # TODO: ack mssg that it moved the offset val 
-        #    pass 
+
         current_position = received_message[0].split(":")[2]
         fiducials_detected += 1
         data[f"{fiducials_detected}"] = current_position
         time.sleep(1)
 
-    message = f"MOVE_MOTOR_STEPS:0,0,{boundary_offset}"   # moving to left 
-    send_serial(message)
+    send_serial(f"MOVE_MOTOR_STEPS:0,0,{boundary_offset}") # moving to left 
     wait_for_ack("MOVE_MOTOR_STEPS")
-    #ser.write(message.encode('utf-8')+ b'\n')
-    # request current position and store that as outer boundary 
-    # create helper for returning current position
-    message = "STOP_MOTOR:0"
-    send_serial(message)
-    wait_for_ack("STOP_MOTOR")
 
-    message = "RETURN_POS:0"
-    send_serial(message)
-    #ser.write(message.encode('utf-8')+ b'\n')
-    # while received message split[0]!= CURRENT_POS
+    send_serial("STOP_MOTOR:0")
+    wait_for_ack("STOP_MOTOR")
+    time.sleep(0.01)
+
+    # request current position and store that as outer boundary 
+    # TODO: current position helper
+    send_serial("RETURN_POS:0")
     wait_for_ack("RETURN_POS")
-    #while (received_message[0].split(":")[1]!= 'RETURN_POS'): # TODO: ack mssg that it moved the offset val 
-    #    pass 
-    current_position = received_message.split(":")[1]
-    print(data)
+    current_position = received_message[0].split(":")[2]
+    print(current_position)
+
     
         
 # if __name__ == "__main__":
@@ -205,6 +173,7 @@ if fiducials_detected < total_fiducials:
     calibrate()
 
 if fiducials_detected == total_fiducials:
+    print(data)
     print("All fiducials found!")
 else:
     raise ValueError("Failed to final all fiducials") 
