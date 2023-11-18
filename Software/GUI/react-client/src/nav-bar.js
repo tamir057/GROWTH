a9974242ec2af82623d2272bad3e82216b1fc91b
Rida Jawed <rida.jawed@gmail.com>
2023-11-18 17:09:04 -0500

    nav bar

--- Software/GUI/react-client/src/nav-bar.js
+++ Software/GUI/react-client/src/nav-bar.js
@@ -7,24 +7,31 @@ function NavBar() {
   const [ignore, active] = pathname.split("/");
   return (
     <div className="w-100 pos-fixed bg-primary-green">
+      <div className="row"> 
+      <div className="col-3 p-2 ml-10">
+        <h3>|        GROWTH robot</h3>
+      </div>
+      <div className="col-9"> 
       <ul className="nav nav-pills mb-2 mt-2 float-end">
-        <li className="nav-item">
-          <Link
-            to="/garden"
-            className={`nav-link ${active === "about" ? "active" : ""}`}
-          >
-            Garden
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/plantprofile"
-            className={`nav-link ${active === "home" ? "active" : ""}`}
-          >
-            Profile
-          </Link>
-        </li>
-      </ul>
+          <li className="nav-item">
+            <Link
+              to="/garden"
+              className={`white-text nav-link ${active === "about" ? "active" : ""}`}
+            >
+              Garden
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/plantprofile"
+              className={`white-text nav-link ${active === "home" ? "active" : ""}`}
+            >
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+      </div>
     </div>
   );
 }
