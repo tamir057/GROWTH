05923e851b09ca0f8b1105630712898af85f8886
tamir057 <tas.amir57@gmail.com>
2023-11-25 18:59:24 -0500

    Functioning login/register. Does not save activity to account

--- Software/GUI/react-client/src/Login_Register/Login.js
+++ Software/GUI/react-client/src/Login_Register/Login.js
@@ -7,82 +7,98 @@ import { useNavigate } from 'react-router-dom';
 
 
 function Login() {
-  const [data, setData] = useState([{}]);
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [password, setPassword] = useState("");
-  const [loggedIn, setLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
-  
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  useEffect(() => {
-    fetch("/members")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
 
-  const onButtonClick = () => {
-    navigate('/garden');
+  const onButtonClick = async () => {
+    try {
+      // Basic client-side validation
+      if (!email || !password) {
+        setError("Email and password are required");
+        return;
+      }
+
+      // Make a POST request to login the user
+      const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: email,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Login successful, navigate to the garden page
+        navigate('/garden');
+      } else {
+        setError(data.error || "Invalid username or password");
+      }
+    } catch (error) {
+      setError("An unexpected error occurred");
+    }
   };
+
 
   return (
     <div style={{ backgroundColor: '#CFE1C9' }}>
-    <div className = 'navBar'> </div>
-    <div className="login">
-       
-      <div className={"loginMainContainer"}>
-        <div className={"loginTitleContainer"}>
-          <div>Login To</div>
+      <div className='navBar'> </div>
+      <div className="login">
+
+        <div className={"loginMainContainer"}>
+          <div className={"loginTitleContainer"}>
+            <div>Login To</div>
+          </div>
+          <div className={"loginSubtitleContainer"}> GROWTH Web App</div>
+          <br />
+          <div className={"loginInputContainer"}>
+            <div className={"loginSubtitle2Container"}> Email</div>
+            <input
+              value={email}
+              placeholder="Enter your email here"
+              onChange={(ev) => setEmail(ev.target.value)}
+              className={"loginInputBox"}
+            />
+          </div>
+          <br />
+          <div className={"loginInputContainer"}>
+            <div className={"loginSubtitle2Container"}> Password </div>
+            <input
+              type="password"
+              value={password}
+              placeholder="Enter your password here"
+              onChange={(ev) => setPassword(ev.target.value)}
+              className={"loginInputBox"}
+            />
+          </div>
+          <br />
+          <div className={"loginInputContainer"}>
+            <input
+              className={"buttonLogin"}
+              type="button"
+              onClick={onButtonClick}
+              value={"Log In"}
+            />
+          </div>
+          <div className={"loginSubtitle3Container"}>
+            {" "}
+            <p>
+              Don't Have an Account?
+              <Link to="/register"> Register</Link>{" "}
+            </p>
+          </div>
         </div>
-        <div className={"loginSubtitleContainer"}> GROWTH Web App</div>
-        <br />
-        <div className={"loginInputContainer"}>
-          <div className={"loginSubtitle2Container"}> Email</div>
-          <input
-            value={email}
-            placeholder="Enter your email here"
-            onChange={(ev) => setEmail(ev.target.value)}
-            className={"loginInputBox"}
-          />
-          <label className="errorLabel">{emailError}</label>
-        </div>
-        <br />
-        <div className={"loginInputContainer"}>
-          <div className={"loginSubtitle2Container"}> Password </div>
-          <input
-            type="password"
-            value={password}
-            placeholder="Enter your password here"
-            onChange={(ev) => setPassword(ev.target.value)}
-            className={"loginInputBox"}
-          />
-          <label className="errorLabel">{passwordError}</label>
-        </div>
-        <br />
-        <div className={"loginInputContainer"}>
-          <input
-            className={"buttonLogin"}
-            type="button"
-            onClick={onButtonClick}
-            value={"Log In"}
-          />
-        </div>
-        <div className={"loginSubtitle3Container"}>
-          {" "}
-          <p>
-            Don't Have an Account?
-            <Link to="/register"> Register</Link>{" "}
-          </p>
+        <div>
+          <img src={hydroponics2} height={500} width={800} />
         </div>
       </div>
-      <div>
-        <img src={hydroponics2} height={500} width={800} />
-      </div>
-    </div>
     </div>
   );
 }
