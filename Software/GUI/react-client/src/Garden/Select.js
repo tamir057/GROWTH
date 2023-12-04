52971f9a00a008c99d5b5f79e3ecbce37a79f33c
tamir057 <tas.amir57@gmail.com>
2023-12-03 21:54:31 -0500

    hard coded -- UNDO AFTER VID

--- Software/GUI/react-client/src/Garden/Select.js
+++ Software/GUI/react-client/src/Garden/Select.js
@@ -210,7 +210,7 @@ const Select = ({ showModal, handleClose, plotNumber }) => {
                   value={sensorReadings.ec}
                   minValue={0}
                   maxValue={14}
-                  customSegmentStops={[0, 1.2, 2.1, 14]}
+                  customSegmentStops={[1, 1.2, 2.1, 3]}
                   segmentColors={['#FF6961', '#B4D3B2', '#FF6961']}
                   currentValueText={`pH: ${sensorReadings.ec}`}
                 />
